4da9c9a3d2a3e4e2ee1cae70844e0e9db820fdfe
Brandon McGhee <bmcghee39@gmail.com>
2021-01-02 17:53:28 -0500

    commit after starting tailwind

---
+++ resources/js/PostList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container mx-auto px-4 ">PostList
+      <div v-if="$apollo.loading"></div>
+      <div v-else>
+          <div v-for="post in posts" :key="post.id">
+              {{post.id }} is titled {{post.title }}
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag';
+
+export default {
+    apollo: {
+        posts: gql`{
+            posts{
+                id
+                title
+            }
+        }`
+    }
+}
+</script>
+
+<style>
+
+</style>
